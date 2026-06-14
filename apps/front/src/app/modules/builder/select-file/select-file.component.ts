7d658f923e6edfe67e7580461c7226b6d4cee9bb
akalitenya <akalitenya@mprove.io>
2026-06-14 10:47:00 +0000

    s1056

--- apps/front/src/app/modules/builder/select-file/select-file.component.ts
+++ apps/front/src/app/modules/builder/select-file/select-file.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { map } from 'rxjs/operators';
+import { APP_SPINNER_NAME } from '#common/constants/top-front';
 
 @Component({
   standalone: false,
   selector: 'm-select-file',
   templateUrl: './select-file.component.html'
 })
-export class SelectFileComponent {}
+export class SelectFileComponent {
+  isAppSpinnerVisible$ = this.spinner
+    .getSpinner(APP_SPINNER_NAME)
+    .pipe(map(spinner => spinner.show));
+
+  constructor(private spinner: NgxSpinnerService) {}
+}
